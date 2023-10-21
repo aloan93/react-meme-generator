0d012584b52a9f393f20753363ca67896685346d
aloan93 <a.loan93@gmail.com>
2023-10-21 17:25:34 +0100

    fully implemented ability to chose between uploading image or image url - added reset button to clear canvas

--- src/components/MemeForm.jsx
+++ src/components/MemeForm.jsx
@@ -2,14 +2,21 @@ import { useState } from "react";
 
 function MemeForm({ setInputs }) {
   const [isUpload, setIsUpload] = useState(false);
+  const [file, setFile] = useState(null);
 
   function toggleInput() {
     isUpload ? setIsUpload(false) : setIsUpload(true);
   }
 
+  function handleFile(e) {
+    setFile(URL.createObjectURL(e.target.files[0]));
+  }
+
   function getInputs(e) {
     e.preventDefault();
-    setInputs([e.target[0].value, e.target[1].value, e.target[2].value]);
+    if (!isUpload)
+      setInputs([e.target[0].value, e.target[1].value, e.target[2].value]);
+    else setInputs([e.target[0].value, e.target[1].value, file]);
   }
 
   return (
@@ -54,7 +61,12 @@ function MemeForm({ setInputs }) {
             <div className="input" id="url-text">
               <label>
                 Image URL:
-                <input type="text" name="image-url" placeholder="Image URL" />
+                <input
+                  type="text"
+                  name="image-url"
+                  placeholder="Image URL"
+                  required
+                />
               </label>
             </div>
           )}
@@ -62,14 +74,21 @@ function MemeForm({ setInputs }) {
             <div className="input" id="image-upload">
               <label>
                 Upload Image:
-                <input type="file" name="image-upload" />
+                <input
+                  onChange={handleFile}
+                  type="file"
+                  name="image-upload"
+                  required
+                />
               </label>
             </div>
           )}
         </div>
-        <div className="form-buttons">
-          <button>Submit</button>
-          <button type="reset">Clear</button>
+        <div className="form-button-container">
+          <button className="form-buttons">Submit</button>
+          <button className="form-buttons" type="reset">
+            Clear
+          </button>
         </div>
       </form>
     </>
